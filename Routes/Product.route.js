97beb3ec2b4c2934004a9731857bd315457f646c
Hogan Vu <huyhoangsetup@gmail.com>
2023-01-16 02:53:49 +0700

    update rest.http and Product.route.js

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -6,4 +6,20 @@ router.get('/', (req,res) => {
     res.send("Getting a list of all products...")
 })
 
+router.post('/', (req, res) => {
+    res.send("Product created")
+})
+
+router.get('/:id', (req, res) => {
+    res.send("Getting a single product")
+})
+
+router.patch('/:id', (req, res) => {
+    res.send("Updating a single product")
+})
+
+router.delete('/:id', (req, res) => {
+    res.send("Deleting a single product")
+})
+
 module.exports = router;
